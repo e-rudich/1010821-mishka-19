d9184eecef0d7fef1badf3d735f76c5f61ecad4e
Elena <e.d.rudich@gmail.com>
2020-05-05 17:28:52 +0300

    расширяет кликабельность инпутов, фиксирует максимальную ширину на десктопе

--- source/js/modal.js
+++ source/js/modal.js
@@ -8,19 +8,15 @@ for (var i = 0; i < cart.length; i++) {
   }
 )};
 
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
+function closeModal(evt) {
+  if (evt.target === modal || evt.keyCode === 27) {
     evt.preventDefault();
     if (modal.classList.contains("item-modal--show")) {
       modal.classList.remove("item-modal--show");
     }
   }
-});
+};
 
-modal.addEventListener("click", function (evt) {
-  if (evt.target === modal) {
-    if (modal.classList.contains("item-modal--show")) {
-      modal.classList.remove("item-modal--show");
-    }
-  }
-});
+window.addEventListener("keydown", closeModal);
+
+modal.addEventListener("click", closeModal);
